bd2b5d21abb1d7246176f58e29096b358b0e5f7e
haeseoky <haeseoky@kyobobook.co.kr>
2019-02-28 09:02:30 +0900

    1. twoSum 2. ReverseInterger add problems

---
+++ src/TwoSum.java
@@ -0,0 +1,18 @@
+public class TwoSum {
+//    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//    Example:
+//    Given nums = [2, 7, 11, 15], target = 9,
+//    Because nums[0] + nums[1] = 2 + 7 = 9,
+//    return [0, 1].
+
+
+    public TwoSum() {
+        twoSum(new int[]{1, 2, 3, 4, 5}, 5);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        return new int[]{1,2};
+    }
+}
